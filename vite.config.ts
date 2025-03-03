8589643a0c81a6a900780f1e27091aded6e501ce
Shadow Studio <149867434+shadowstudio1969@users.noreply.github.com>
2025-03-03 15:02:36 +0530

    Update vite.config.ts
    satvik

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/shadowstudio1969"
+  base:"/shadowstudio1969/",
 
   optimizeDeps: {
     exclude: ['lucide-react'],
